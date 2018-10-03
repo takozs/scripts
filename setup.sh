0f5fc24a55126d1072588cf7be34ad0a76e94b29
takozs <43068074+takozs@users.noreply.github.com>
2018-10-03 20:00:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 
 sudo apt-get update -y
 sudo apt-get upgrade -y 
-sudo apt-get install mc nmap apache synaptic -y
+sudo apt-get install mc nmap apache2 synaptic nano screenfetch -y
